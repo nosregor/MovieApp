aea7d4096f59a0bbc4cb33dd4ed435a5b8a4b5c6
Andrew Rogerson <nosregor@users.noreply.github.com>
2023-10-10 13:53:05 +0200

    move main ui to a composable

--- app/src/main/java/com/example/movieapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/MainActivity.kt
@@ -4,81 +4,16 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.movieapp.ui.theme.MovieAppTheme
+import com.example.movieapp.ui.screens.Home
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("ProduceStateDoesNotAssignValue")
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println("onCreate()")
 
         setContent {
-
-            val viewModel: MainViewModel = viewModel()
-            val state by viewModel.state.collectAsState()
-
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Scaffold(
-                        topBar = { TopAppBar(title = { Text(text = "Rick and Morty") }) }
-                    ) { innerPadding ->
-                        when (state) {
-                            MainViewModel.UiState.Loading -> {
-                                Box(modifier = Modifier.fillMaxSize()) {
-                                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                                }
-                            }
-                            is MainViewModel.UiState.Success -> {
-                                LazyVerticalGrid(
-                                    columns = GridCells.Adaptive(180.dp),
-                                    modifier = Modifier.padding(innerPadding)
-                                ) {
-                                    items((state as MainViewModel.UiState.Success).data) { character ->
-                                        Text(
-                                            text = character.name,
-                                            color = if(character.favorite) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.onBackground,
-                                            modifier = Modifier
-                                                .padding(vertical = 8.dp)
-                                                .clickable {
-                                                    viewModel.onCharacterClicked(character)
-                                                })
-                                    }
-                                }
-                            }
-                            is MainViewModel.UiState.Error -> {
-                                Box(modifier = Modifier.fillMaxSize()) {
-                                    Text((state as MainViewModel.UiState.Error).message ?: "Network error", modifier = Modifier.align(Alignment.Center))
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            Home()
         }
     }
 
@@ -107,20 +42,3 @@ class MainActivity : ComponentActivity() {
         println(onDestroy())
     }
 }
-
-// TODO: delete
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    MovieAppTheme {
-//        Greeting("Android")
-//    }
-//}
